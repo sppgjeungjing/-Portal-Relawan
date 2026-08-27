e187587cbfaea91f888f25f32ca217a58d4713d8
a <faturrizkyfahriansyah12@gmail.com>
2026-08-28 03:40:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // Lihat README.md → "Langkah 2 — Setup Google Apps Script".
 //
 // Contoh format URL yang benar:
-// https://script.google.com/macros/s/AKfycb..................../exec
+// https://script.google.com/macros/s/AKfycb.................../exec
 // ============================================================
 
 const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby5uuwdo0G-W22txE7eOSuHuFGc_XYnrhm7SyGvLsbXApJiJNE4eM5nuFYJ29uXM8yMIw/exec';
