059f57d0dacd11721347e7c90573e29b6b303f95
a <faturrizkyfahriansyah12@gmail.com>
2026-08-27 12:32:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 // https://script.google.com/macros/s/AKfycb..................../exec
 // ============================================================
 
-const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'GOOGLE_APPS_SCRIPT_WEB_APP_URL';
+const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby5uuwdo0G-W22txE7eOSuHuFGc_XYnrhm7SyGvLsbXApJiJNE4eM5nuFYJ29uXM8yMIw/exec';
 
 // Set true sementara di perangkat Anda sendiri untuk melihat log setiap
 // request API (action, durasi, status) di Console browser — TIDAK PERNAH
