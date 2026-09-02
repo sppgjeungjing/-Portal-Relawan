62c3fafcc88a0a855a8279d5e2c585dfdd6fbedd
a <faturrizkyfahriansyah12@gmail.com>
2026-09-02 18:58:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ============================================================
 // SPPG JEUNGJING — KONFIGURASI SISTEM ABSENSI
 // ============================================================
-const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyMOXMtHP2MPGBiOwLQ3M_7l-CzNCN7FD_oMB7yFPXudoOUwDCvG9WOo9dwEPqW-bQizg/exec';
+const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby5uuwdo0G-W22txE7eOSuHuFGc_XYnrhm7SyGvLsbXApJiJNE4eM5nuFYJ29uXM8yMIw/exec';
 
 // ============================================================
 // SIPANDU — Web App URL Apps Script TERPISAH
